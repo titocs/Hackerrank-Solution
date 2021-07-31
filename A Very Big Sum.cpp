8f4b2d4d072e9a2be8f18929481ad0467504a603
Hfn <55591161+titocs@users.noreply.github.com>
2021-07-31 14:24:07 +0700

    Create A Very Big Sum.cpp

---
+++ A Very Big Sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main(){
+    ll n, count = 0; cin >> n;
+    ll arr[n];
+    for(ll i=0; i<n; ++i){
+        cin >> arr[i];
+    }
+    for(ll i=0; i<n; ++i){
+        count += arr[i];
+    }
+    cout << count;
+}
