3527a216680bc03479cbb605da043caa2ac860b9
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-01 22:12:06 +0700

    Create Number Line Jumps.cpp

---
+++ Number Line Jumps.cpp
@@ -0,0 +1,14 @@
+// link problem: https://www.hackerrank.com/challenges/kangaroo/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x1, v1, x2, v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    if((x2 - x1) * (v2 - v1) < 0 && (x2 - x1) % (v2 - v1) == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+
+}
