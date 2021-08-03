1773ab269e6fc6844d786b9963d4fa7db725003d
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-03 23:38:28 +0700

    Create Minimum Distances.cpp

---
+++ Minimum Distances.cpp
@@ -0,0 +1,27 @@
+// Link problem: https://www.hackerrank.com/challenges/minimum-distances/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int count = 0;
+    vector<int> result;
+    vector<int> arr(n);
+    for(int i=0; i<n; ++i){
+        cin >> arr[i];
+    }
+    for(int i=0; i<n; ++i){
+        for(int j=i+1; j<n; ++j){
+            if(arr[i] == arr[j]){
+                int temp = abs(i-j);
+                result.push_back(temp);
+                count++;
+            }
+        }
+    }
+    if(count == 0)
+        cout << "-1";
+    else
+        cout << *min_element(result.begin(), result.end());
+}
