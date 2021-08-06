d4048eb77b392e9a3dbbd5d3a515df30fa25dc28
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-06 21:49:21 +0700

    Update Funny String.cpp

--- Funny String.cpp
+++ Funny String.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void reverseStr(string& str){
     int n = str.length();
     for (int i = 0; i < n / 2; i++)
-        swap(str[i], str[n - i - 1]);
+        swap(str[i], str[n-i-1]);
 }
 
 int main(){
