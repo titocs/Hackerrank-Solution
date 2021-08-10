6bd9c9bf681d23d48d40482cc8cf8b23a6ad9ba9
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-11 00:36:26 +0700

    Create HackerRank in a String.cpp

---
+++ HackerRank in a String.cpp
@@ -0,0 +1,24 @@
+// Link problem: https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    string temp = "hackerrank";
+    while(n--){
+        string s; cin >> s;
+        int count = 0;
+        int j = 0;
+        for(int i=0; i<s.length(); ++i){
+            if(s[i] == temp[j]){
+                j++;
+                count++;
+            }
+        }
+        if(count >= 10)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;     
+    }
+}
