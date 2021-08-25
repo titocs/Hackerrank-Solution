e4f6802624f63909d9ba9e195131e163f30b0eff
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-25 12:53:50 +0700

    Create Ice Cream Parlor.cpp

---
+++ Ice Cream Parlor.cpp
@@ -0,0 +1,29 @@
+// Link problem: 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t, n;
+    cin >> t;
+    while(t--){
+        int k; cin >> k >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; ++i){
+            cin >> arr[i];
+        }
+        bool flag = false;
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<n; ++j){
+                if(i == j)
+                    continue;
+                if(arr[i] + arr[j] == k && !flag){
+                    cout << i + 1 << " " << j + 1 << endl;
+                    flag = true;
+                    continue;
+                }
+            }
+        }
+    }
+
+}
