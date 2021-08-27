f6dc34062747adecae9f6312604455fa505e8287
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-28 00:19:32 +0700

    Update Pangrams.cpp

--- Pangrams.cpp
+++ Pangrams.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 int c[26];
 int main(){
-	string s; getline(cin,s);
-	for(int i=0; i<s.length(); i++) {
+    string s; getline(cin,s);
+    for(int i=0; i<s.length(); i++) {
         if(s[i] == ' ')
-			continue;
-		if('A' <= s[i] && s[i] <= 'Z')
-			c[s[i] - 'A']++;
-		else
-			c[s[i] - 'a']++;
-	}
-	for(int i=0; i<26; i++) {
-		if(c[i] == 0) {
-			cout << "not pangram" << endl;
-			return 0;
-		}
-	}
-	cout << "pangram" << endl;
-	return 0;
+            continue;
+        if('A' <= s[i] && s[i] <= 'Z')
+            c[s[i] - 'A']++;
+        else
+            c[s[i] - 'a']++;
+    }
+    for(int i=0; i<26; i++) {
+        if(c[i] == 0) {
+            cout << "not pangram" << endl;
+            return 0;
+        }
+    }
+    cout << "pangram" << endl;
+    return 0;
 }
