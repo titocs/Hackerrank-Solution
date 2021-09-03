06fa24a0e739e247f843630ab56bcdb6d75c3ff6
Hfn <55591161+titocs@users.noreply.github.com>
2021-09-03 11:09:08 +0700

    Create Find Digits.cpp

---
+++ Find Digits.cpp
@@ -0,0 +1,18 @@
+// Link problem: https://www.hackerrank.com/challenges/find-digits/problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n, count = 0; cin >> n;
+        int temp = n;
+        while(temp > 0){
+            if(temp % 10 != 0 && n % (temp % 10) == 0)
+                count++;
+            temp /= 10;
+        }
+        cout << count << '\n';
+    }
+}https://www.hackerrank.com/challenges/find-digits/problem
